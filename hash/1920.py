d0cd1274a2e648ad2fafdbb210dd4502f7260f12
seoyoon lee <leeseoyoon01@gmail.com>
2025-02-24 13:39:18 +0900

    BOJ 1920

--- hash/1920.py
+++ hash/1920.py
@@ -1,6 +1,11 @@
-N = int(input())
-A = list(map(int, input().split())) 
-M = int(input())
-X = list(map(int, input().split())) 
+import sys
+input = sys.stdin.readline  # 한 줄씩 입력받기
+
+N = int(input().strip())  # 첫 번째 입력: N
+A = set(input().split())  # 두 번째 입력: A (set으로 변환)
+
+M = int(input().strip())  # 세 번째 입력: M
+X = input().split()  # 네 번째 입력: X 리스트
+
 for x in X:
     print(1 if x in A else 0)
