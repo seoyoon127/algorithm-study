34e53e278b0df3085a55482b45d1a80b4126af75
seoyoon lee <leeseoyoon01@gmail.com>
2025-02-28 22:28:16 +0900

    BOJ 2493

--- stack/2493.py
+++ stack/2493.py
@@ -1,22 +1,13 @@
 N = int(input())
 nums = list(map(int, input().split()))
-nums2 = []
+stack = []
+index = []
 answer = []
-answer2 = []
-for i in range(int(N/2)+1):
-    x = nums.pop()
-    if len(nums) >= i+1:
-        nums2.append(nums[i])
-    if len(nums2) >= i+1:
-        if nums2[i] == max(nums2):
-            answer2.append(0)
-        else:
-            answer2.append(nums.index(max(nums2)))
-    prev = -1
-    while -prev <= len(nums) and nums[prev] < x:
-        prev -= 1
-    if -prev > len(nums):
-        answer.append(0)
-    else:
-        answer.append(N-i+prev) 
-print(" ".join(map(str, sorted(answer2+answer)))) 
+for i in range(N):
+    x = nums[i]
+    while len(stack) > 0 and x > stack[-1][0]:
+        stack.pop()
+    answer.append(stack[-1][1]+1 if stack else 0)
+    stack.append((x,i))
+
+print(" ".join(map(str, answer)))
