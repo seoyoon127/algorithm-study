87540dccb4c19803178f0ded8eb2b91e801ca56f
seoyoon lee <leeseoyoon01@gmail.com>
2025-03-10 20:38:03 +0900

    BOJ 11659

--- others/11659.py
+++ others/11659.py
@@ -4,7 +4,13 @@ data = input().splitlines()
 
 N, M = map(int, data[0].split())
 arr = list(map(int, data[1].split()))
+S = [0]
+temp = 0
+
+for i in arr:
+    temp += i
+    S.append(temp)
 
 for i in range(M):
     left, right = map(int, data[i+2].split())
-    print(sum(arr[left-1:right])) 
+    print(S[right] - S[left-1]) 
